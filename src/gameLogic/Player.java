ca221ce4b7b69a28e4a704e7af74105e9ee8d4fc
Replit user <>
2023-12-18 23:43:05 +0000

    Logic for player class

---
+++ src/gameLogic/Player.java
@@ -0,0 +1,38 @@
+public class Player extends ShipLocations {
+  
+  public Player(){
+    super();
+  }
+
+  public ShotStatus fireAt(Point coords) {
+      return super.shootLocation(coords); // Leverage inherited method
+  }
+
+  // Convenient method to check if all ships are sunken
+  public boolean hasWon() {
+      return super.getUnguessedSections().isEmpty(); // Leverage inherited data
+  }
+
+  // Convenient method to get number of remaining ship sections
+  public int getRemainingSections() {
+      return super.getUnguessedSections().size(); // Leverage inherited data
+  }
+
+
+  public ArrayList<Point> getMisses(){
+    return super.getMisses(); 
+  }
+
+  // Convenient method to get hit map for visualizing progress
+  public HashMap<Point, ShipSection> getHitMap() {
+      return super.getHitSections(); // Leverage inherited data
+  }
+
+  public void viewUserGrid(){
+    //Output a users own grid
+  }
+
+  public void viewShots(){
+    //output a user's hit and misses of the bot's ships
+  }
+}
