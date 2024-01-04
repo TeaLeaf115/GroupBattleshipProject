bb4accd8549ba25f22650eb7e97f5d276490fdcb
TeaLeaf115 <91578545+TeaLeaf115@users.noreply.github.com>
2024-01-04 07:00:18 -0500

    Adding a stupid comment

--- src/gameLogic/GamePlayLogic.java
+++ src/gameLogic/GamePlayLogic.java
@@ -13,25 +13,34 @@ public class GamePlayLogic {
 		player = new Player();
 		bot = new Bots(GamePanel.computerDifficulty);
 
-//		// destroyer
-//		Ship playerDestroyer = new Ship(Ship.ShipType.DESTROYER, Ship.Rotation.DOWN);
-//		playerDestroyer.setCoords(0, 0);
-//		player.shipLocations.addUnguessedShip(playerDestroyer);
-//
-//		// submarine
-//		Ship playerSubmarine = new Ship(Ship.ShipType.SUBMARINE, Ship.Rotation.LEFT);
-//		playerSubmarine.setCoords(2, 3);
-//		player.shipLocations.addUnguessedShip(playerSubmarine);
-//
-//		// cruiser
-//		Ship playerCruiser = new Ship(Ship.ShipType.CRUISER, Ship.Rotation.UP);
-//		playerCruiser.setCoords(4, 4);
-//		player.shipLocations.addUnguessedShip(playerCruiser);
-//
-//		// battleship
-//		Ship playerBattleship = new Ship(Ship.ShipType.BATTLESHIP, Ship.Rotation.RIGHT);
-//		playerBattleship.setCoords(5, 5);
-//		player.shipLocations.addUnguessedShip(playerBattleship);
+		/*
+		Ship playerDestroyer = new Ship(Ship.ShipType.DESTROYER, Ship.Rotation.DOWN);
+		playerDestroyer.setCoords(0, 0);
+		player.shipLocations.addUnguessedShip(playerDestroyer);
+
+		// submarine
+		Ship playerSubmarine = new Ship(Ship.ShipType.SUBMARINE, Ship.Rotation.LEFT);
+		playerSubmarine.setCoords(2, 3);
+		player.shipLocations.addUnguessedShip(playerSubmarine);
+
+		// cruiser
+		Ship playerCruiser = new Ship(Ship.ShipType.CRUISER, Ship.Rotation.UP);
+		playerCruiser.setCoords(4, 4);
+		player.shipLocations.addUnguessedShip(playerCruiser);
+
+		// battleship
+		Ship playerBattleship = new Ship(Ship.ShipType.BATTLESHIP, Ship.Rotation.RIGHT);
+		playerBattleship.setCoords(5, 5);
+		player.shipLocations.addUnguessedShip(playerBattleship);
+		
+		bot.shipLocations.shootLocation(new Point(0, 0));
+		bot.shipLocations.shootLocation(new Point(1,1));
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				bot.shipLocations.shootLocation(new Point(i, j));
+			}
+		}
+    */
 		
 		bot.shipLocations.shootLocation(new Point(0, 0));
 		bot.shipLocations.shootLocation(new Point(1,1));
