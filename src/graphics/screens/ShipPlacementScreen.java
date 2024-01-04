62168684f4f55617d03686b4303e6c4b47cf0353
Kevin <kevinbczhang@gmail.com>
2024-01-03 19:20:49 -0500

    Fixing Rotation Bug

--- src/graphics/screens/ShipPlacementScreen.java
+++ src/graphics/screens/ShipPlacementScreen.java
@@ -20,51 +20,51 @@ public class ShipPlacementScreen extends JPanel {
     private final BufferedImage shipPlacementScreen;
     private Dimension screenSize;
     private Point screenLocation, originPoint;
-    
+
     // drag components
     private final ArrayList<DragAndDropHandler> dragComponents;
     public static GamePlayLogic gameLogic = GameplayScreen.gl;
-    
+
     public ShipPlacementScreen() {
         this.setBackground(new Color(0x848482));
         this.setSize(new Dimension(1274, 699));
-        
+
         // screen
         this.shipPlacementScreen = GamePanel.sm.getPlacementOverlay();
         this.screenSize = new Dimension(
                 (int) Math.floor(shipPlacementScreen.getWidth() * getSpriteScaleMultiplier()),
                 (int) Math.floor(shipPlacementScreen.getHeight() * getSpriteScaleMultiplier()));
-        
+
         // location where the screen is drawn
         this.screenLocation = new Point(
                 this.getWidth() / 2 - this.screenSize.width / 2,
                 this.getHeight() / 2 - this.screenSize.height / 2);
-        
+
         // location where the board starts
         this.originPoint = new Point(
                 (int) Math.floor(this.screenLocation.x + this.screenSize.width * 2 / 7),
                 (int) Math.floor(this.screenLocation.y + this.screenSize.height * 3 / 14));
-        
+
         // drag components
         this.dragComponents = new ArrayList<>();
         Player player = ShipPlacementScreen.gameLogic.player;
-        
+
         for (Ship ship : player.getShips()) {
             DragAndDropHandler dragComponent = new DragAndDropHandler(ship, player, this.originPoint);
             this.dragComponents.add(dragComponent);
             this.add(dragComponent.getShipLabel()); // add the ship JLabel directly
         }
-        
+
     }
-    
+
     public void draw() {
         this.repaint();
     }
-    
-    public void paintComponent(Graphics g) {
+
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-        
+
         g2.drawImage(
                 this.shipPlacementScreen,
                 this.screenLocation.x,
@@ -72,19 +72,34 @@ public class ShipPlacementScreen extends JPanel {
                 this.screenSize.width,
                 this.screenSize.height,
                 null);
-        
-        // g2.drawRect(
-        //         this.originPoint.x,
-        //         this.originPoint.y,
-        //         10 * GamePanel.scaledTileSize,
-        //         10 * GamePanel.scaledTileSize);
+
+        for (DragAndDropHandler dragComponent : this.dragComponents) {
+            BufferedImage shipImage = dragComponent.getImg();
+            Point labelCoords = dragComponent.getShipLabel().getLocation();
+
+            g2.drawImage(
+                    shipImage,
+                    labelCoords.x,
+                    labelCoords.y,
+                    shipImage.getWidth(),
+                    shipImage.getHeight(),
+                    null);
+
+            g2.setColor(Color.RED);
+            g2.drawRect(
+                labelCoords.x, 
+                labelCoords.y, 
+                shipImage.getWidth(), 
+                shipImage.getHeight());
+        }
+
     }
-    
+
     public void update() {
         // this.screenLocation.setLocation(
         // this.getWidth() / 2 - this.screenSize.width / 2,
         // this.getHeight() / 2 - this.screenSize.height / 2);
-        
+
         // Add update logic if needed
     }
 }
